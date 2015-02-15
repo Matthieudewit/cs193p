03ef327de0c4bc0de256ef6ef4e192acf15f13bf
Vojta Molda <vojta.molda@gmail.com>
2015-02-15 16:28:41 -0600

    Lecture 5 - Happiness

---
+++ Hapiness/Hapiness/HapinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HapinessViewController.swift
+//  Hapiness
+//
+//  Created by Vojta Molda on 2/14/15.
+//  Copyright (c) 2015 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class HapinessViewController: UIViewController {
+
+
+}
