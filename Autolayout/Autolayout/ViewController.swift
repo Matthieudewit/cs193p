95c1cd71beb54853f767582141c1c90efe2039bd
Vojta Molda <vojta.molda@gmail.com>
2015-03-10 17:52:14 -0500

    Lecture 8 - Autolayout

---
+++ Autolayout/Autolayout/ViewController.swift
@@ -0,0 +1,97 @@
+//
+//  ViewController.swift
+//  Autolayout
+//
+//  Created by Vojta Molda on 3/9/15.
+//  Copyright (c) 2015 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var companyLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    var loggedInUser: User? {
+        didSet { updateUI() }
+    }
+    var secure: Bool = false {
+        didSet { updateUI() }
+    }
+    var aspectRatioConstraint: NSLayoutConstraint? {
+        willSet {
+            if let existingConstraint = aspectRatioConstraint {
+                view.removeConstraint(existingConstraint)
+            }
+        }
+        didSet {
+            if let newConstraint = aspectRatioConstraint {
+                view.addConstraint(newConstraint)
+            }
+        }
+    }
+    var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            if let constrainedView = imageView {
+                if let newImage = newValue {
+                    aspectRatioConstraint = NSLayoutConstraint(
+                        item: constrainedView,
+                        attribute: .Width,
+                        relatedBy: .Equal,
+                        toItem: constrainedView,
+                        attribute: .Height,
+                        multiplier: newImage.aspectRatio,
+                        constant: 0)
+                } else {
+                    aspectRatioConstraint = nil
+                }
+            }
+        }
+    }
+
+    
+    private func updateUI() {
+        passwordField.secureTextEntry = secure
+        passwordLabel.text = secure ? "Secure Password" : "Password"
+        nameLabel.text = loggedInUser?.name
+        companyLabel.text = loggedInUser?.company
+        image = loggedInUser?.image
+    }
+    
+    @IBAction func login() {
+        loggedInUser = User.login(loginField.text ?? "", password: passwordField.text ?? "")
+        
+    }
+
+    @IBAction func toggleSecurity() {
+        secure = !secure
+    }
+}
+
+
+extension User {
+    var image: UIImage? {
+        return UIImage(named: login) ?? UIImage(named: "unknown_user")
+    }
+}
+
+extension UIImage {
+    var aspectRatio: CGFloat {
+        return size.height != 0 ? size.width/size.height : 0
+    }
+}
