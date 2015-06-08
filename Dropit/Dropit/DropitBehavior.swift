ac0eadc07e95b40a44054a1b5a6e3a5b0f788568
Vojta Molda <vojta.molda@gmail.com>
2015-06-07 21:08:10 -0500

    Lecture 12 - Dropit

---
+++ Dropit/Dropit/DropitBehavior.swift
@@ -0,0 +1,53 @@
+//
+//  DropitBehavior.swift
+//  Dropit
+//
+//  Created by Vojta Molda on 6/7/15.
+//  Copyright (c) 2015 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class DropitBehavior: UIDynamicBehavior {
+    
+    let gravity = UIGravityBehavior()
+    
+    lazy var collider: UICollisionBehavior = {
+        let lazilyCreatedCollisionBehavior = UICollisionBehavior()
+        lazilyCreatedCollisionBehavior.translatesReferenceBoundsIntoBoundary = true
+        return lazilyCreatedCollisionBehavior
+    }()
+    
+    lazy var dropBehavior: UIDynamicItemBehavior = {
+        let lazilyCreatedDropBehavior = UIDynamicItemBehavior()
+        lazilyCreatedDropBehavior.allowsRotation = true
+        lazilyCreatedDropBehavior.elasticity = 0.75
+        return lazilyCreatedDropBehavior
+    }()
+
+    override init() {
+        super.init()
+        addChildBehavior(gravity)
+        addChildBehavior(collider)
+        addChildBehavior(dropBehavior)
+    }
+    
+    func addBarrier(path: UIBezierPath, named name: String) {
+        collider.removeBoundaryWithIdentifier(name)
+        collider.addBoundaryWithIdentifier(name, forPath: path)
+    }
+    
+    func addDrop(drop: UIView) {
+        dynamicAnimator?.referenceView?.addSubview(drop)
+        gravity.addItem(drop)
+        collider.addItem(drop)
+        dropBehavior.addItem(drop)
+    }
+
+    func removeDrop(drop: UIView) {
+        gravity.removeItem(drop)
+        collider.removeItem(drop)
+        dropBehavior.removeItem(drop)
+        drop.removeFromSuperview()
+    }
+}
