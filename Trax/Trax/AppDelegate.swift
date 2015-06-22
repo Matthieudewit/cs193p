4753e00430217ed83595bdaf69e158c6352d26c0
Vojta Molda <vojta.molda@gmail.com>
2015-06-21 20:14:36 -0500

    Lecture 13 - Trax

---
+++ Trax/Trax/AppDelegate.swift
@@ -0,0 +1,32 @@
+
+
+
+//
+//  AppDelegate.swift
+//  Trax
+//
+//  Created by Vojta Molda on 6/21/15.
+//  Copyright (c) 2015 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+struct GPXURL {
+    static let Notification = "GPXURL Radio Station"
+    static let Key = "GPXURL URL Key"
+}
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, handleOpenURL url: NSURL) -> Bool {
+        let center = NSNotificationCenter.defaultCenter()
+        let notification = NSNotification(name: GPXURL.Notification, object: self, userInfo: [GPXURL.Key:url])
+        center.postNotification(notification)
+        return true
+    }
+
+}
+
